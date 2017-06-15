356997355db65e53f9a5f1b2de1ecf7eb27324ef
Artem <khaapt@gmail.com>
2017-06-15 19:50:13 +0300

    data and url-params

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -5,9 +5,8 @@ import {
   Redirect } from 'react-router-dom';
 
 // App's components
-import CSS from './courses/CSS';
-import HTML from './courses/HTML';
-import JavaScript from './courses/JavaScript';
+import CourseContainer from './courses/CourseContainer';
+import { HTMLCourses, CSSCourses, JSCourses } from '../data/courses';
 
 const Courses = ({match}) => (
   <div className="main-content courses">
@@ -20,9 +19,12 @@ const Courses = ({match}) => (
       </ul>
     </div>
     <Route exact path={match.path} render={ () => <Redirect to={`${match.url}/html`} />}/>
-    <Route path={`${match.path}/css`} component={CSS}/>
-    <Route path={`${match.path}/html`} component={HTML}/>
-    <Route path={`${match.path}/javacript`} component={JavaScript}/>
+    <Route path={`${match.path}/css`}
+      render={() => <CourseContainer data={CSSCourses}/>}/>
+    <Route path={`${match.path}/html`}
+      render={() => <CourseContainer data={HTMLCourses}/>}/>
+    <Route path={`${match.path}/javacript`}
+      render={() => <CourseContainer data={JSCourses}/>}/>
   </div>
 );
 
